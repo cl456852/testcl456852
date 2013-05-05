5010129eaa925948abcb1b5fbab9c2f81ddb11c5
cl456852@gmail.com <cl456852@gmail.com@39bb2fc9-c57f-507d-fb8e-d1f8a18c63eb>
2013-05-05 15:23:37 +0000

    修改查找HIS 的SQL为大小差在一定范围内

--- trunk/video filter - 副本 - 副本/DB/DBHelper.cs
+++ trunk/video filter - 副本 - 副本/DB/DBHelper.cs
@@ -9,7 +9,7 @@ namespace DB
 {
     public class DBHelper
     {
-        static string seachHisSql = "select * from his1 where LOWER(vid)=LOWER('{0}') and size={1} ";
+        static string seachHisSql = "select * from his1 where LOWER(vid)=LOWER('{0}') and ABS((size-{1})/{1})<0.05 ";
         static string insertHisSql = "insert into his1 values('{0}',{1},'{2}',{3},'{4}',getdate())";
         public static string connstr = @"server=localhost\SQLEXPRESS;uid=sa;pwd=a;database=cd";
         //static string connstr = "server=MICROSOF-8335F8\\SQLEXPRESS;uid=sa;pwd=a;database=cd";
