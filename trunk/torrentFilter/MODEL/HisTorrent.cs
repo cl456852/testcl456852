8d0e9a54d77c300436adb518802c87aa1f3be663
cl456852@gmail.com <cl456852@gmail.com@39bb2fc9-c57f-507d-fb8e-d1f8a18c63eb>
2013-07-14 12:16:12 +0000

    添加BTFILTER项目

---
+++ trunk/torrentFilter/MODEL/HisTorrent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MODEL
+{
+    public class HisTorrent
+    {
+        int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        string file;
+
+        public string File
+        {
+            get { return file; }
+            set { file = value; }
+        }
+        double size;
+
+        public double Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+        string path;
+
+        public string Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
+        DateTime createTime;
+
+        public DateTime CreateTime
+        {
+            get { return createTime; }
+            set { createTime = value; }
+        }
+
+        string ext;
+
+        public string Ext
+        {
+            get { return ext; }
+            set { ext = value; }
+        }
+
+    }
+}
